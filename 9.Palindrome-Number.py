e2c0c9edde50410d5ed509fb6d81c00a79be9a51
Mehmet Said Hüseyinoğlu <dasxfc@gmail.com>
2024-04-02 22:35:56 +0300

    9. Palindrome Number (easy)

--- 9.Palindrome-Number.py
+++ 9.Palindrome-Number.py
@@ -1,4 +1,7 @@
-'''def iterate_digits(number):
+'''
+# class Solution:
+
+def iterate_digits(number):
     
     number_str = str(number)
 
@@ -8,7 +11,10 @@
 
 iterate_digits(123456789)'''
 
-'''def isPalindrome(number):
+'''
+# class Solution:
+
+def isPalindrome(number):
 
     number_str = str(number)
 
@@ -16,28 +22,32 @@ iterate_digits(123456789)'''
 
 print(isPalindrome(12321))
 print(isPalindrome(12345))'''
+class Solution:
+    def isPalindrome(self, number):
+        # We added self as the first parameter in the isPalindrome method within the Solution class, as required for instance methods in Python classes
 
-def isPalindrome(number):
-
-    # Convert the given number to a string
-    number_str = str(number)
+        # Convert the given number to a string
+        number_str = str(number)
     
-    # Determine the length of the number
-    length = len(number_str)
+        # Determine the length of the number
+        length = len(number_str)
     
-    # Iterate through the first half of the digits to compare with the corresponding digits from the end
-    for i in range(length // 2):
+        # Iterate through the first half of the digits to compare with the corresponding digits from the end
+        for i in range(length // 2):
         
-        # Check whether the digit at index i from the beginning matches the digit at index length - i - 1 from the end
-        # If the two digits don't match, the number is not a palindrome, so return False immediately
-        if number_str[i] != number_str[length - i - 1]: 
-            return False
+            # Check whether the digit at index i from the beginning matches the digit at index length - i - 1 from the end
+            # If the two digits don't match, the number is not a palindrome, so return False immediately
+            if number_str[i] != number_str[length - i - 1]: 
+                return False
 
-    # If all corresponding digits match, the number is a palindrome, so return True
-    return True
+        # If all corresponding digits match, the number is a palindrome, so return True
+        return True
 
-print(isPalindrome(12321))         # Output: True
-print(isPalindrome(12345))         # Output: False
-print(isPalindrome(12345654321))   # Output: True
+solution = Solution()
+print(solution.isPalindrome(12321))         # Output: True
+print(solution.isPalindrome(12345))         # Output: False
+print(solution.isPalindrome(12345654321))   # Output: True
+print(solution.isPalindrome(12345654322))   # Output: False
+
 
 
